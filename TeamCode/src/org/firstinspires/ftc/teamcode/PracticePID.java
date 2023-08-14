f58d47d24dab54e524deaff18d1364312bf7935b
bavula028 <135271061+bavula028@users.noreply.github.com>
2023-08-14 13:34:18 -0500

    update

--- TeamCode/src/org/firstinspires/ftc/teamcode/PracticePID.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/PracticePID.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -12,7 +12,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@Autonomous (name = "Practice PID")
+@TeleOp (name = "Practice PID")
 public class PracticePID extends LinearOpMode{
 
     private double kP = 0;  //Difference between setPoint and processVariable
@@ -21,17 +21,23 @@ public class PracticePID extends LinearOpMode{
     private double setPoint = 1500;
     private double processVariable = 0;
     private double time = 0;    //Set it equal to getRunTime() later while (opModeIsActive)
-
-
-
+    private double error = 0;
+    private double previousError = 0;
+    private double currentPosition = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
 
+
         waitForStart();
 
         while(opModeIsActive()){
+            time = getRuntime();
+            telemetry.addData("time:", time);
+            telemetry.update();
+
+            currentPosition = 0;
 
         }
 
