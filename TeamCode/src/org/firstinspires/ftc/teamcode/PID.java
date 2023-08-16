a5cfcae01fe7ecd946c06e800ff687f82d7a654e
bavula028 <135271061+bavula028@users.noreply.github.com>
2023-08-16 14:52:58 -0500

    update

--- TeamCode/src/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/PID.java
@@ -1,24 +1,11 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public abstract class PID extends LinearOpMode {
+public abstract class PID extends LinearOpMode{
 
-    public double kP;
-    public double kI;
-    public double kD;
-    public double proportional;
-    public double integral;
-    public double setPoint;
-    public double processVariable;
-    public double time;
-    public double error;
-    public double previousError;
-    public double currentPosition;
-    public double output;
 
+    @Override
     public abstract void runOpMode() throws InterruptedException;
-
 }
--- TeamCode/src/org/firstinspires/ftc/teamcode/RotationPID.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/RotationPID.java
@@ -1,7 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.*;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+
+@TeleOp(name = "Rotation PID")
 public class RotationPID extends PID{
     //This class is applicable for Arm Bot and Mecanum Bot.
 
@@ -11,7 +20,7 @@ public class RotationPID extends PID{
     double proportional = 0;
     double integral = 0;
     double derivative = 0;
-    double setPoint = 0;
+    double setPoint = 90;
     double processVariable = 0;
     double time = 0;
     double error = 0;
@@ -27,18 +36,48 @@ public class RotationPID extends PID{
         DcMotor fl = hardwareMap.dcMotor.get("front_left_motor");
         DcMotor fr = hardwareMap.dcMotor.get("front_right_motor");
 
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationData = null;
+        parameters.calibrationDataFile = " ";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = " ";
+
+
+
         waitForStart();
 
         while (opModeIsActive()) {
 
             time = getRuntime();
-
+            currentAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS).firstAngle * 180 / Math.PI;
             error = setPoint -  currentAngle;
-            //Error
 
             proportional = error;
             integral = integral + error * time;
+            derivative = (error - previousError) / time;
+            output = kP + proportional + kI * integral + kD * derivative;
 
+            telemetry.addData("time:", time);
+            telemetry.addData("current angle:", currentAngle);
+            telemetry.addData("error:", error);
+            telemetry.addData("proportional:", proportional);
+            telemetry.addData("integral:", integral);
+            telemetry.addData("derivative:", derivative);
+            telemetry.addData("output:", output);
+            telemetry.update();
+
+            if (gamepad1.left_bumper) {
+
+                while (imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS).firstAngle * 180 / Math.PI < output){
+                    bl.setPower(-1);
+                    br.setPower(1);
+                    fl.setPower(-1);
+                    fr.setPower(1);
+                }
+            }
         }
 
 
