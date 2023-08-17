7736d287db11123d5b2f17f6912d6576cc5030be
bavula028 <135271061+bavula028@users.noreply.github.com>
2023-08-17 13:34:26 -0500

    update

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/PID.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+public abstract class PID extends LinearOpMode{
+
+    @Override
+    public abstract void runOpMode() throws InterruptedException;
+
+}
