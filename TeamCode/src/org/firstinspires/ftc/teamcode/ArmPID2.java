fef01dbeabe73bca7dba43a5f362cddf0efae6c4
bavula028 <135271061+bavula028@users.noreply.github.com>
2023-08-18 15:30:52 -0500

    update

--- TeamCode/src/org/firstinspires/ftc/teamcode/ArmPID2.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/ArmPID2.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -12,11 +11,12 @@ public class ArmPID2 extends PID{
 
     double kP = 0;
     double kI = 0;
-    double kD = 0;
+    double kD = 1;
     double proportional = 0;
     double integral = 0;
     double derivative = 0;
-    double setPoint = 0;
+    double setPoint = 2000;
+    double processVariable = 0;
     double error;
     double previousError;
     double currentPosition;
@@ -27,12 +27,21 @@ public class ArmPID2 extends PID{
     public void runOpMode() throws InterruptedException {
         DcMotor arm = hardwareMap.dcMotor.get("arm_motor");
 
-        ElapsedTime elapsedTime = new ElapsedTime();
+        //arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //ElapsedTime elapsedTime = new ElapsedTime();
 
         waitForStart();
 
         while (opModeIsActive()){
-            time = elapsedTime.seconds();
+            //time = elapsedTime.seconds();
+
+
+            //currentPosition is always returning 0, even after the arm extends.
+            //Why?
+
+            time = getRuntime();
             currentPosition = arm.getCurrentPosition();
             error = setPoint - currentPosition;
 
@@ -42,13 +51,35 @@ public class ArmPID2 extends PID{
 
             output = kP * proportional + kI * integral + kD * derivative;
 
-            telemetry.addData("time:", time);
-            telemetry.addData("current position:", currentPosition);
-            telemetry.addData("error:", error);
-            telemetry.addData("proportional:", proportional);
-            telemetry.addData("integral:", integral);
-            telemetry.addData("derivative:", derivative);
-            telemetry.addData("output:", output);
+            while (currentPosition < output){
+                arm.setPower(1);
+
+                time = getRuntime();
+                currentPosition = arm.getCurrentPosition();
+                error = setPoint - currentPosition;
+
+                proportional = error;
+                integral = integral * error + time;
+                derivative = (error - previousError) / time;
+
+                output = kP * proportional + kI * integral + kD * derivative;
+
+                telemetry.addData("time:", getRuntime());
+                telemetry.addData("current position:", arm.getCurrentPosition());
+                telemetry.addData("error:", error);
+                telemetry.addData("proportional:", proportional);
+                telemetry.addData("integral:", integral);
+                telemetry.addData("derivative:", derivative);
+                telemetry.addData("output:", output);
+                telemetry.update();
+
+            }
+
+            telemetry.addData("arm current position:", arm.getCurrentPosition());
+            telemetry.update();
+
+
+            //requestOpModeStop();
 
 
         }
